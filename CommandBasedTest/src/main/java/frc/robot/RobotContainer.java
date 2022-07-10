68e8a65b9b57e7e4f9a24c06022c09b842fd73fb
Vernonicuhhh <urmumalad@gmail.com>
2022-07-09 23:12:53 -0700

    fix: working drive code sim

--- CommandBasedTest/src/main/java/frc/robot/RobotContainer.java
+++ CommandBasedTest/src/main/java/frc/robot/RobotContainer.java
@@ -6,17 +6,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.DriveFieldOriented;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.Intaking.IntakeBall;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.Indexer;
-import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -31,8 +25,6 @@ public class RobotContainer {
   XboxController driver = new XboxController(0);
 
   private final Drive drive = new Drive();
-  private final Intake intake = new Intake();
-  private final Indexer indexer = new Indexer();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
@@ -52,12 +44,6 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(driver, Button.kY.value).whenPressed(
-      new InstantCommand(intake::toggleIntake, intake));
-    
-    new JoystickButton(driver, Button.kB.value).whenPressed(
-      new IntakeBall(indexer, intake)
-    );
   }
 
   /**
